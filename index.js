03e230b2a3d178b96dab6f47f6f29505fb2e8395
Ray Templo <rtemplo@gmail.com>
2018-04-18 14:19:53 -0400

    installed mongoose

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send({hi: 'stuff'});
-});
-
 app.listen(PORT);
